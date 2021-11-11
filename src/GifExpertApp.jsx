c982b97ffbfdb11ae3fbe84d2d0814c497f48ffb
Luis Villén <l.villenjaldon@gmail.com>
2021-11-11 19:45:02 +0100

    Creates AddCategory component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import { useState, } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['Lord of the Rings', 'Star Wars', 'Game of Thrones']);
+
+    // const handleAdd = () => {
+    //   setCategories([...categories, 'Riverdale']);
+    // };
+
     return(
         <>
             <h2>Gif Expert App</h2>
+            <AddCategory setCategoriesProp={ setCategories }/>
             <hr />
+
+            <ul>
+              {categories.map( category => <li key={ category }> { category } </li>)}
+            </ul>
         </>
-    )
-}
+    );
+};
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+
+export const AddCategory = ( { setCategoriesProp } ) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = e => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    
+    if (inputValue.trim().length > 2) {
+      setCategoriesProp(categories => [...categories, inputValue]);
+      setInputValue('');
+    };
+  };
+
+  return (
+    <>
+      <form onSubmit={ handleSubmit }>
+        <input
+          type="text"
+          value={ inputValue }
+          onChange={ handleInputChange }
+        />
+      </form>
+    </>
+  );
+};
+
+AddCategory.propTypes = {
+  setCategoriesProp: PropTypes.func.isRequired
+};
